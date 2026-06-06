58015b0c3ec4650543b06dc25f01b3027f237a92
SaraCA12 <scalderon@unal.edu.co>
2026-06-05 21:14:22 -0500

    Update Green SQL Queries for Staging, Duplicated and Anomalies elimination

--- pipelines/data_transformation/models/staging/green/stg_green.sql
+++ pipelines/data_transformation/models/staging/green/stg_green.sql
@@ -115,18 +115,94 @@ normalized_columns AS (
 )
 
 
--- 2. Filter anomalies in dates 
+-- 3. Filter anomalies in dates 
 correct_date AS (
     SELECT *
-    FROM green_source
-    WHERE pick_
-
+    FROM normalized_columns
+    WHERE EXTRACT(year from tpep_pickup_datetime) = {{var("anio")}}
+    AND EXTRACT(year from  tpep_dropoff_datetime) = {{var("anio")}}
 )
 
-SELECT nombre || apellido || cast(edad as varchar(2)) AS nombre_completo
-FROM clientes;
+-- 4. Delete Duplicated
+
+duplicated_delete AS (
+  SELECT *
+  FROM (SELECT *,
+        ROW_NUMBER() OVER (
+          PARTITION BY
+            vendor_id,
+            tpep_pickup_datetime,
+            tpep_dropoff_datetime,
+            pu_location_id,
+            do_location_id
+          ORDER BY tpep_pickup_datetime) AS rn
+      FROM correct_date)
+  WHERE rn = 1
+)
+
+-- 5. Delete Anomalies in trip distance and total amount
+
+delete_amount_anomalies AS (
+  SELECT *
+  FROM duplicated_delete
+  WHERE total_amt >=0 AND trip_distance>=0
+)
+
+-- 6. Delete Date anomalies
+
+delete_date_anomalies AS(
+  SELECT *
+  FROM delete_amount_anomalies
+  WHERE tpep_dropoff_datetime>= tpep_pickup_datetime
+)
+
+-- 7. Nulls management ratecode_id and Amount Columns
+
+nulls_first_filter AS (
+    SELECT vendor_id, tpep_pickup_datetime, tpep_dropoff_datetime,
+        COALESCE(ratecode_id, 1) AS ratecode_id,
+        pu_location_id, do_location_id, 
+
+        CASE
+          WHEN passenger_count IS NULL
+          THEN CASE WHEN FLOOR(AVG(passenger_count))
+          ELSE passenger_count
+        END AS passenger_count, 
+
+        trip_distance, fare_amount, extra, mta_tax, tip_amt, tolls_amt,
+        COALESCE(congestion_surcharge, 0) AS congestion_surcharge,
+        cbd_congestion_fee, improvement_surcharge,
+        total_amt, true_total_amt, comparation_total_amt,
+      
+        CASE
+          WHEN payment_type IS NULL
+          THEN CASE WHEN (
+            SELECT COUNT(payment_type) 
+            FROM delete_date_anomalies
+            GROUP BY payment_type
+            ORDER BY COUNT(payment_type) DESC
+            LIMIT 1)
+          ELSE payment_type
+        END AS payment_type, 
+        
+        CASE
+          WHEN trip_type IS NULL
+          THEN CASE WHEN RAND() <=  0.97 THEN 1 ELSE 2 END 
+          ELSE trip_type
+        END AS trip_type
+       FROM delete_date_anomalies
+)
 
 
 -- FInal select
 SELECT *
 FROM limpio
+
+
+
+
+SELECT columna, COUNT(*) AS frecuencia
+FROM tabla
+GROUP BY columna
+ORDER BY frecuencia DESC
+LIMIT 1;
