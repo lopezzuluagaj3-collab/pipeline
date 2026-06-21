c97d2c806365c6d07a22b547056c8e40aaf73c64
lopezzuluagaj3-collab <lopezzuluagaj3@gmail.com>
2026-06-21 12:25:27 -0500

    Update fhv_orquestador.py

--- pipelines/dags/fhv_orquestador.py
+++ pipelines/dags/fhv_orquestador.py
@@ -1,6 +1,8 @@
 from airflow import DAG
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.python import PythonOperator
+from airflow.api.client.local_client import Client
 from datetime import datetime, timezone
+import time
 
 def generar_periodos(anio_inicio, anio_fin):
     periodos = []
@@ -12,25 +14,37 @@ def generar_periodos(anio_inicio, anio_fin):
             periodos.append((anio, mes))
     return periodos
 
+def ejecutar_pipeline(dag_id, anio_inicio, anio_fin, **context):
+    client = Client(None, None)
+    for anio, mes in generar_periodos(anio_inicio, anio_fin):
+        run_id = f'orq_{anio}_{mes:02d}_{datetime.now().strftime("%Y%m%d%H%M%S")}'
+        client.trigger_dag(
+            dag_id=dag_id,
+            run_id=run_id,
+            conf={'anio': anio, 'mes': mes}
+        )
+        # Esperar a que termine antes de disparar el siguiente
+        while True:
+            state = client.get_dag_run_state(dag_id=dag_id, run_id=run_id)
+            if state in ('success', 'failed'):
+                break
+            if state == 'failed':
+                raise Exception(f'Failed: {anio}-{mes:02d}')
+            time.sleep(60)
+
 with DAG(
     dag_id='fhv_orquestador',
     schedule=None,
     start_date=datetime(2024, 1, 1, tzinfo=timezone.utc),
     catchup=False,
-    max_active_tasks=1,
     tags=['fhv', 'orquestador'],
 ) as dag:
-    anterior = None
-    for anio, mes in generar_periodos(2015, 2026):
-        t = TriggerDagRunOperator(
-            task_id=f'trigger_{anio}_{mes:02d}',
-            trigger_dag_id='fhv_staging_pipeline',
-            conf={'anio': anio, 'mes': mes},
-            wait_for_completion=True,
-            poke_interval=60,
-            allowed_states=['success'],
-            failed_states=['failed'],
-        )
-        if anterior:
-            anterior >> t
-        anterior = t
+    PythonOperator(
+        task_id='ejecutar_todos_los_meses',
+        python_callable=ejecutar_pipeline,
+        op_kwargs={
+            'dag_id': 'fhv_staging_pipeline',
+            'anio_inicio': 2015,
+            'anio_fin': 2026,
+        },
+    )
